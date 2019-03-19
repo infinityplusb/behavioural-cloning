e56ccbf759386cc7cd066e01617f28b6a6398c98
brian <brian@infinityplusb.com>
2019-03-20 01:44:24 +1100

    Model 08

--- clone.py
+++ clone.py
@@ -3,43 +3,89 @@ import cv2
 import os
 import numpy as np
 
-#os.environ["CUDA_VISIBLE_DEVICES"]="1"
-#import tensorflow as tf
+os.environ["CUDA_VISIBLE_DEVICES"]="1"
+import tensorflow as tf
+#run_opts = tf.RunOptions(report_tensor_allocations_upon_oom = True)
 
 input_path = '/home/brian/Documents/Training/Udacity/Self_Driving_Car/beta_simulator_linux/beta_simulator_Data/'
+image_path = input_path + 'IMG/'
 
 lines = []
 with open(os.path.join(input_path, 'driving_log.csv')) as csvfile:
     reader = csv.reader(csvfile)
     for line in reader:
         lines.append(line)
-        
 
-#for line in lines:
 from sklearn.utils import shuffle
 
 import sklearn
 from sklearn.model_selection import train_test_split
-train_samples, validation_samples = train_test_split(lines, test_size=0.2)
+train_samples, validation_samples = train_test_split(lines, test_size=0.1)
+train_samples = train_samples[1:] 
 
-print(len(train_samples))
-print(len(validation_samples))
+#### Split out the data into seperate train and valid directories
+import shutil
+def move_files_to_folder(my_images, image_type) :
+    location_to_make = './' + image_type + 'ing_images'
+    if not os.path.exists(location_to_make):
+        os.mkdir(location_to_make)
+        
+    if location_to_make.count('./') > 1:
+        shutil.rmtree(location_to_make, ignore_errors=False)
+        os.makedirs(location_to_make)
+        print("Successfully cleaned directory " + location_to_make)
+     
+    for images in my_images :
+        for i in range(3) :
+#            print(i)
+#            print(images[i])
+#            print(location_to_make + '/')
+            shutil.copy(images[i], location_to_make + '/')
 
-def generator(samples, batch_size = 32):
+#move_files_to_folder(train_samples, 'train')
+#move_files_to_folder(validation_samples, 'valid')
+
+from keras.preprocessing.image import ImageDataGenerator
+        
+datagen = ImageDataGenerator(
+        validation_split=0.2)
+
+#        rotation_range=40,
+#        width_shift_range=0.2,
+#        height_shift_range=0.2,
+#        rescale=1./255,
+#        shear_range=0.2,
+#        zoom_range=0.2,
+#        horizontal_flip=True,
+#        fill_mode='nearest')
+
+train_generator = datagen.flow_from_directory(
+        image_path , subset='training')
+
+val_generator = datagen.flow_from_directory(
+        image_path , subset='validation')
+
+
+def generator(samples, type1, batch_size = 32):
     num_samples = len(samples)
-    #print(num_samples)
+    if type1 == 'valid' :
+        indices = np.random.randint(0, batch_size, num_samples)
+        samples = np.array(samples)[indices]
+    correction = 0.2
+    
     while 1:
         shuffle(samples)
         for offset in range(0, num_samples, batch_size):
+
             batch_images = samples[offset:offset+batch_size]
-            #print("number of images in this batch: ", len(batch_images))
+
             images = []
             measurements = []
+
             for line in batch_images:
-                correction = 0.2
                 # do the centre image
                 image = cv2.imread(line[0])
-                measurement = float(line[3])
+                measurement = np.round(np.float16(line[3]),2) + .01
                 images.append(image)
                 measurements.append(measurement)
                 # flip the centre image
@@ -48,47 +94,33 @@ def generator(samples, batch_size = 32):
 
                 # do the left image
                 image = cv2.imread(line[1])
-                measurement = float(line[3]) + correction
+                measurement = np.float16(line[3]) + correction
+                imageCount  += 1
                 images.append(image)
                 measurements.append(measurement)
                 # flip the left image
                 images.append(cv2.flip(image, 1))
                 measurements.append(measurement*-1.0)
 
-                # do the centre image
+                # do the right image
                 image = cv2.imread(line[2])
-                measurement = float(line[3]) - correction
+                measurement = np.float16(line[3]) - correction
+                imageCount  += 1
                 images.append(image)
                 measurements.append(measurement)
-                # do the centre image
+                # flip the right image
                 images.append(cv2.flip(image, 1))
                 measurements.append(measurement*-1.0)
-            
-            #print(offset)
-            #print("Length: " , len(measurements))
-            X_train = np.array(images)
-            y_train = np.array(measurements)
-            #print(X_train.shape)
-            #print(y_train.shape)
-                #print("Taking a break at %d: " , offset)
-            yield sklearn.utils.shuffle(X_train, y_train)
-
-batch_size = 128
-
-train_generator = generator(train_samples, batch_size=batch_size)
-validation_generator = generator(validation_samples, batch_size=batch_size)    
-
-#augmented_images, augmented_measurements = [], []
-#for image, measurement in zip(images, measurements):
-#    augmented_images.append(image)
-#    augmented_measurements.append(measurement)
-#    augmented_images.append(cv2.flip(image,1))
-#    augmented_measurements.append(measurement*-1.0)
+                
+                X_train = np.array(images)
+                y_train = np.array(measurements)
+                yield shuffle(X_train, y_train)
 
 
-#X_train = np.array(augmented_images)
-#y_train = np.array(augmented_measurements)
+batch_size = 8
 
+train_generator = generator(train_samples, type1='train', batch_size=batch_size)
+validation_generator = generator(validation_samples, type1='valid', batch_size=batch_size)
 
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda
@@ -100,64 +132,40 @@ model = Sequential()
 model.add(Lambda(lambda x: x/255.0 - 0.5, input_shape=(160, 320, 3))) #, output_shape=(160, 320, 3)))
 model.add(Cropping2D(cropping=((70,25), (0,0))))
 
-model.add(Conv2D(24,(5,5), activation='relu')) #, strides=(2,2)))
-#model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2)))
-#model.add(BatchNormalization())
-
+model.add(Conv2D(24,(5,5), activation='relu'))
 model.add(Conv2D(36,(5,5), activation='relu'))
-#model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2)))
-#model.add(BatchNormalization())
-#model.add(Conv2D(24,(5,5), activation='relu'))
-#model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2)))
-#model.add(BatchNormalization())
-
-#model.add(Conv2D(36,(5,5), activation='relu'))
-#model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2)))
-#model.add(BatchNormalization())
-
 model.add(Conv2D(48,(5,5), activation='relu'))
-#model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2)))
-#model.add(BatchNormalization())
-
+model.add(Conv2D(64,(3,3), activation='relu'))
 model.add(Conv2D(64,(3,3), activation='relu'))
 #model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2)))
-
-model.add(Conv2D(64,(3,3), activation='relu'))
-#model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2)))
-
-#model.add(BatchNormalization())
 
 model.add(Flatten())
 
-
-model.add(Dense(1000))
-# Add Dropout to prevent overfitting
-model.add(Dropout(0.4))
-# Batch Normalisation
-#model.add(BatchNormalization())
-
-
-model.add(Dense(500))
-# Add Dropout to prevent overfitting
-model.add(Dropout(0.4))
-# Batch Normalisation
-#model.add(BatchNormalization())
-
 model.add(Dense(50))
-# Add Dropout to prevent overfitting
-model.add(Dropout(0.4))
+model.add(Dropout(0.6))
 # Batch Normalisation
 #model.add(BatchNormalization())
-
 
 model.add(Dense(1))
 
 #with tf.device("/gpu:1"):
-model.compile(loss='mse', optimizer='adam')
-model.fit_generator(train_generator, steps_per_epoch=np.ceil(len(train_samples)/batch_size), validation_data=validation_generator, validation_steps=np.ceil(len(validation_samples)/batch_size), epochs=5, verbose=1)
+model.compile(loss='mse', optimizer='adam')#, options = run_opts)
+
+model.fit_generator(train_generator
+                    #, steps_per_epoch=20 
+                    , np.ceil(len(train_samples)/batch_size)
+                    , validation_data=validation_generator
+                    , validation_steps=np.ceil(len(validation_samples)/batch_size)
+                    #, use_multiprocessing=True
+                    #, workers = 4
+                    , epochs=5
+                    , verbose=1)
+
+print(np.ceil(len(train_samples)/batch_size))
+print(np.ceil(len(validation_samples)/batch_size))
 #model.fit(X_train, y_train, validation_split=0.4, shuffle=True, epochs=10)
 
-model.save('model_06.h5')
+model.save('model_08.h5')
 
 import matplotlib.pyplot as plt
 
